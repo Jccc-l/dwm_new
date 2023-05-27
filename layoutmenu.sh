cc3777c644c5a8e0d38cbbdd3dd0f7316ba1afd1
Jccc-l <1216550215Jc@gmail.com>
2023-05-27 23:32:09 +0800

    Modify layoutmenu.sh

--- layoutmenu.sh
+++ layoutmenu.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
 cat <<EOF | xmenu
-[]= Tiled Layout	0
-><> Floating Layout	1
-[M] Monocle Layout	2
+[]=  tile                   0
+[M]  monocle                1
+[@]  spiral                 2
+[\\] dwindle                3
+H[]  deck                   4
+TTT  bstack                 5
+===  bstackhoriz            6
+HHH  grid                   7
+###  nrowgrid               8
+---  horizgrid              9
+:::  gaplessgrid            10
+|M|  centeredmaster         11
+>M>  centeredfloatingmaster 12
+><>  NULL                   13
 EOF
